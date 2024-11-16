bfd42d6a50948281a9f1c40e864e4be7f7f4ad10
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-16 16:06:19 +0100

    by gacp

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
-const colorsArray =
-[
+// const colorsArray =
+// [
+// 	"#2C3E50",
+// 	"#34495E",
+// 	"#2C2C2C",
+// 	"#616A6B",
+// 	"#4A235A",
+// 	"#2F4F4F",
+// 	"#0E4B5A",
+// 	"#36454F",
+// 	"#2C3E50",
+// 	"#800020",
+// 	"#767e8a",
+// 	"#afcf23",
+// 	"#cf7323",
+// 	"#e86f05",
+
+// ]
+
+// const body = document.querySelector("body");
+// const hex = document.querySelector("#hex");
+
+
+// function getColorIndex()
+// {
+// 	return (Math.floor(colorsArray.length * Math.random()));
+// }
+
+
+// function changeBackgroundColor()
+// {
+// 	const color = colorsArray[getColorIndex()];
+	
+// 	hex.innerText = color;
+// 	body.style.backgroundColor = color;
+	
+// }
+
+
+// const btn = document.querySelector("#btn");
+// btn.onclick = changeBackgroundColor();
+
+
+const darkColorsArr = [
 	"#2C3E50",
 	"#34495E",
 	"#2C2C2C",
@@ -10,33 +52,22 @@ const colorsArray =
 	"#36454F",
 	"#2C3E50",
 	"#800020",
-	"#767e8a",
-	"#afcf23",
-	"#cf7323",
-	"#e86f05",
-
-]
-
-const body = document.querySelector("body");
-const hex = document.querySelector("#hex");
-
-
-function getColorIndex()
-{
-	return (Math.floor(colorsArray.length * Math.random()));
-}
-
-
-function changeBackgroundColor()
-{
-	const color = colorsArray[getColorIndex()];
-	
-	hex.innerText = color;
+  ];
+  
+  function getRandomIndex()
+  {
+	const randomIndex = Math.floor(darkColorsArr.length * Math.random());
+	return randomIndex;
+  }
+  
+  const body = document.querySelector("body");
+  const bgHexCodeSpanElement = document.querySelector("#bg-hex-code");
+  
+  function changeBackgroundColor()
+  {
+	const color = darkColorsArr[getRandomIndex()];
+  
+	bgHexCodeSpanElement.innerText = color;
 	body.style.backgroundColor = color;
-	
-}
-
-
-const btn = document.querySelector("#btn");
-btn.onclick = changeBackgroundColor();
-
+  }
+  const btn = document.querySelector("#btn");
