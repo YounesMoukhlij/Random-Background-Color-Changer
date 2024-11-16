43532162e7d3abf5ddf4b2f3820cfdb2ad7438dc
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-16 15:21:28 +0100

    by gacp

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const colorsArray =
 
 const body = document.querySelector("body");
 const hex = document.querySelector("#hex");
-const btn = document.querySelector(".btnn");
+const btn = document.querySelector("btnn");
 
 
 function getColorIndex()
